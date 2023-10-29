5d39a2b3ebca74558d37ad82287e3ab387d0b770
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2023-10-29 17:23:58 +0100

    Final version 1.0

--- server.js
+++ server.js
@@ -3,86 +3,83 @@ const cors = require('cors');
 const fs = require('fs');
 const app = express();
 const port = 3001;
+const json5 = require('json5');
+app.use(express.json());
 
 app.use(cors()); 
 
-//let items = loadItemsFromFile();
-
-let items = [
-    {
-      "taskID": 1,
-      "taskTitle": "Task 1",
-      "taskDescription": "Description for Task 1",
-      "isActive": false
-    },
-    {
-      "taskID": 2,
-      "taskTitle": "Task 2",
-      "taskDescription": "Description for Task 2",
-      "isActive": false
-    },
-    {
-      "taskID": 3,
-      "taskTitle": "Task 3",
-      "taskDescription": "Description for Task 3",
-      "isActive": false
-    }
-  ]
-
+let items = loadItemsFromFile();
 app.use(express.json());
 
-app.get('/api/tasks', (req, res) => {
-    const formattedItems = items.map(item => ({
-        taskID: parseInt(item.taskID, 10), 
-        taskTitle: item.taskTitle,
-        taskDescription: item.taskDescription,
-        isActive: item.isActive
-    }));
-    res.json(formattedItems);
-    saveItemsToFile();
-    loadItemsFromFile();
+app.get('/api/tasks/', (req, res) => {
+  const formattedItems = items.map(item => ({
+      taskID: parseInt(item.taskID,10),
+      taskTitle: item.taskTitle,
+      taskDescription: item.taskDescription,
+      isActive: item.isActive
+  }));
+  res.json(formattedItems);
 });
 
-app.post('/api/tasks', (req, res) => {
-    const newItem = req.body;
-    newItem.taskID = parseInt(newItem.taskID, 10);
+//read
+app.get('/api/tasks/:id', (req, res) => {
+    const taskId = req.params.id;
+    const task = items.find(item => item.taskID === taskId);
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404).json({ error: 'Task not found' });
+    }
+
+});
+
+//create
+app.post('/api/tasks/', (req, res) => {
+  const jsonString = JSON.stringify(req.body);
+  const newItem = JSON.parse(jsonString);
+  const isTaskIDUnique = items.every(item => item.taskID !== newItem.taskID);
+  if (!isTaskIDUnique) {
+      return res.status(400).json({ error: 'TaskID must be unique' });
+  }else{
     items.push(newItem);
+    saveItemsToFile();
     res.json(newItem);
-    saveItemsToFile();
-    loadItemsFromFile();
+    console.log(newItem);
+  }
 });
 
+//update/replace
 app.put('/api/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id, 10);
-    const updatedItem = req.body;
-    items = items.map(item => (item.taskID === taskId ? updatedItem : item));
-    res.json(updatedItem);
-    saveItemsToFile();
-    loadItemsFromFile();
+  const taskId = req.params.id;
+  const updatedItem = req.body;
+  const itemIndex = items.findIndex(item => item.taskID === taskId);
+  items[itemIndex] = updatedItem;
+  saveItemsToFile();
+  res.json(updatedItem);
+  console.log(updatedItem);
 });
 
+//delete
 app.delete('/api/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id, 10);
-    const deletedItem = items.find(item => item.taskID === taskId);
-    items = items.filter(item => item.taskID !== taskId);
-    res.json(deletedItem);
-    saveItemsToFile();
-    loadItemsFromFile();
+  const taskId = req.params.id;
+  const deletedItem = items.find(item => item.taskID === taskId);
+  items = items.filter(item => item.taskID !== taskId);
+  saveItemsToFile();
+  res.json(deletedItem);
+  console.log(deletedItem);
 });
 
 function saveItemsToFile() {
-    const data = JSON.stringify(items, null, 2);
-    fs.writeFileSync('items.json', data);
+  const data = JSON.stringify(items, null, 2);
+  fs.writeFileSync('items.json', data);
 }
 
 function loadItemsFromFile() {
-    const data = fs.readFileSync('items.json', 'utf8');
-    const items = JSON.parse(data);
-   return items; 
+  const data = fs.readFileSync('items.json', 'utf8');
+  const items = JSON.parse(data);
+ return items; 
 }
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
-console.log(items);
